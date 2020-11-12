2b39ebfd5d2b1a43cd15bc5cc6ebfe9053709eca
shadman-a <shadman@outlook.com>
2020-11-12 18:32:02 -0500

    2147483647

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	    int myValue = 10000;
 	    int myMinIntValue = Integer.MIN_VALUE;
 	    int myMaxIntValue = Integer.MAX_VALUE;
+		int myMaxIntValueTest = 2_147_483_647;
 	    System.out.println("Integer Minimum Value =" + myMinIntValue);
         System.out.println("Integer Maximum Value =" + myMaxIntValue);
     }
