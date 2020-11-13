719ccfbc9d502e95739b0a914b1ce9e11e821a36
shadman-a <shadman@outlook.com>
2020-11-12 20:10:30 -0500

    int is the way to go

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,27 @@ public class Main {
 		int myMaxIntValueTest = 2_147_483_647;
 	    System.out.println("Integer Minimum Value =" + myMinIntValue);
         System.out.println("Integer Maximum Value =" + myMaxIntValue);
-    }
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+		byte myMaxByteValue = Byte.MAX_VALUE;
+
+		System.out.println(myMaxByteValue);
+		System.out.println(myMinByteValue);
+
+		short myMinShortValue = Short.MIN_VALUE;
+		short myMaxShortValue = Short.MAX_VALUE;
+
+		System.out.println(myMaxShortValue);
+		System.out.println(myMinShortValue);
+		
+		long myLongValue = 100L;
+
+		long myMinLongValue = Long.MIN_VALUE;
+		long myMaxLongValue = Long.MAX_VALUE;
+
+		System.out.println(myMaxLongValue);
+		System.out.println(myMinLongValue);
+		
+		byte myNewByteValue = (byte) (myMinByteValue / 2);
+	}
 }
